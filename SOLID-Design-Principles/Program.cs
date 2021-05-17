cac353caa8a7690e7393ba9d2c83c38f5855a314
Arun Tyagi <aruntyagi@Aruns-MacBook-Pro.local>
2021-05-17 13:35:27 +0530

    folder structure correction

--- SOLID-Design-Principles/Program.cs
+++ SOLID-Design-Principles/Program.cs
@@ -7,35 +7,9 @@ namespace SOLID_Design_Principles
     class Program
     {
 
-
-        static void Single_Responsibility_Principle_Client()
-        {
-            try
-            {
-                Todo todo = new Todo();
-
-                #region [ Add todo ]
-                todo.AddEntry("Complete Solid Principle");
-                todo.AddEntry("Complete Builder patterns");
-                todo.AddEntry("Complete Behavioral pattern");
-                #endregion
-
-                Persistence p = new Persistence();
-                #region [ Persistence ]
-
-                p.SaveToFile(todo, "/tasklist.txt");
-
-                #endregion
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-        }
-
         static void Main(string[] args)
         {
-            Single_Responsibility_Principle_Client();
+            SRP_Client.Single_Responsibility_Principle_Client();
         }
     }
 }
---
+++ SOLID-Design-Principles/Single Responsibility Principle/SRP_Client.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace Single_Responsibility_Principle
+{
+    class SRP_Client
+    {
+       public static void Single_Responsibility_Principle_Client()
+        {
+            try
+            {
+                Todo todo = new Todo();
+
+                #region [ Add todo ]
+                todo.AddEntry("Complete Solid Principle");
+                todo.AddEntry("Complete Builder patterns");
+                todo.AddEntry("Complete Behavioral pattern");
+                #endregion
+
+                Persistence p = new Persistence();
+                #region [ Persistence ]
+
+                p.SaveToFile(todo, "/tasklist.txt");
+
+                #endregion
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
